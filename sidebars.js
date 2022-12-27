9be302c383e0fc343a7d177c797630c18eaf9624
beyzaerf <beyzaerfidan@gmail.com>
2022-12-27 10:20:58 +0300

    feat: change cli location

--- sidebars.js
+++ sidebars.js
@@ -22,12 +22,12 @@ const sidebars = {
     {
       type: "category",
       label: 'Start',
-      items: ['start/getting-started','start/installation','start/where-to-start'],
+      items: ['start/getting-started','start/installation','start/where-to-start', 'concept/cli'],
     },
     {
       type: "category",
       label: 'Concept',
-      items: ['concept/bucket','concept/passport','concept/function','concept/storage','concept/dashboard','concept/user-activities','concept/webhook','concept/l10n','concept/asset-store', 'concept/cli', 'concept/orm'],
+      items: ['concept/bucket','concept/passport','concept/function','concept/storage','concept/dashboard','concept/user-activities','concept/webhook','concept/l10n','concept/asset-store', 'concept/orm'],
     },
     {
       type: "category",
